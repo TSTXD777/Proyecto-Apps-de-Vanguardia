8ed5b57af8d89f62efa1dcc20b88d730896379ec
TSTXD777 <TSTXD777@hotmail.com>
2026-06-04 17:55:22 -0600

    Ejemplo de cargar variables de .env en el código (obviamente sin el .env real)

---
+++ Backend/Sis. Ges. Doc. Backend/EnvironmentVarTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DotNetEnv;
+
+namespace Sis.Ges.Doc.Backend
+{
+    public class EnvironmentVarTest
+    {
+        static void Main()
+        {
+            Env.Load();
+
+            string db_host = Environment.GetEnvironmentVariable("DB_HOST");
+            string db_user = Environment.GetEnvironmentVariable("DB_USER");
+            string db_pass = Environment.GetEnvironmentVariable("DB_PASS");
+
+            Console.WriteLine($"Host: {db_host}");
+            Console.WriteLine($"User: {db_user}");
+            Console.WriteLine($"Pass: {db_pass}");
+
+        }
+    }
+}
