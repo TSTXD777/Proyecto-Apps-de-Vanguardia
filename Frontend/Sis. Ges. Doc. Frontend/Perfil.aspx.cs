f77c932ed25691dd134461f3d5f65db9d4c0d642
TSTXD777 <TSTXD777@hotmail.com>
2026-06-03 18:25:05 -0600

    Creación de archivos por pantallas

--- Frontend/Sis. Ges. Doc. Frontend/Perfil.aspx.cs
+++ Frontend/Sis. Ges. Doc. Frontend/Perfil.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace Sis.Ges.Doc.Frontend
 {
-    public partial class About : Page
+    public partial class Perfil : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
